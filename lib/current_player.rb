658eee2a0c29a985b7da0bcf6c45a1364fad07c8
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 02:09:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(count)
   turns = 0
-  board.each do |space|
+  count.each do |space|
     if space == "X" || space == "O"
       turns += 1
     end
